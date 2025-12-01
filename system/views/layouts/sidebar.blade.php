4b6a272caba3199f82784f62ad2abb03167001db
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-01 16:17:02 -0400

    feat: add sidebar layout with navigation for system modules

--- system/views/layouts/sidebar.blade.php
+++ system/views/layouts/sidebar.blade.php
@@ -159,14 +159,6 @@
                     <div class="nav-icon">
                         <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                             <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
-                        </svg>
-                    </div>
-                    <span class="nav-text">Permisos</span>
-                </a>
-            </li>
-
-            <!-- Configuraciones -->
-            <li class="nav-item">
 <aside class="dashboard-sidebar" id="systemSidebar">
     <!-- Logo del Sidebar -->
     <div class="sidebar-logo">
@@ -383,3 +375,4 @@
 <!-- Incluir CSS del Sidebar -->
 <link rel="stylesheet" href="{{ asset('css/components/sidebar.css') }}">
 <!-- Incluir JS del Sidebar -->
+<script src="{{ asset('js/components/sidebar.js') }}"></script>
