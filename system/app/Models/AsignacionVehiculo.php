ed5a6b84f1c1e206bfb9b28d056666169e4fa674
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-03 17:47:02 -0400

    feat: Create `AsignacionVehiculo` Eloquent model with fillable fields and relationships.

--- system/app/Models/AsignacionVehiculo.php
+++ system/app/Models/AsignacionVehiculo.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AsignacionVehiculo extends Model
 {
+    use \Illuminate\Database\Eloquent\Factories\HasFactory;
     protected $table = 'asignaciones_vehiculo';
 
     protected $fillable = [
